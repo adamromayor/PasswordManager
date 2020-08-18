e73039fac1243d93c046f6106817a5e05a192939
Adam Romayor <adamromayor@Adams-MacBook-Pro.local>
2020-08-17 21:11:55 -0700

    Password manager completed. User can add and update passwords

---
+++ crypto.py
@@ -0,0 +1,42 @@
+import cryptography
+from cryptography.fernet import Fernet
+
+
+keyfile = ".secret.key"
+
+def generate_key():
+    """
+    Generates a key and save it into a file
+    """
+    key = Fernet.generate_key()
+
+    with open(keyfile, "wb") as key_file:
+        key_file.write(key)
+
+
+def load_key():
+    """
+    Loads the key named `secret.key` from the current directory.
+    """
+    return open(keyfile, "rb").read()
+
+
+def encrypt_password(password):
+    """
+    Encrypts a message
+    """
+    key = load_key()
+    encoded_password = password.encode()
+    f = Fernet(key)
+    encrypted_password = f.encrypt(encoded_password)
+    return encrypted_password
+
+
+def decrypt_password(encrypted_password):
+    """
+    Decrypts an encrypted message
+    """
+    key = load_key()
+    f = Fernet(key)
+    decrypted_password = f.decrypt(encrypted_password)
+    return decrypted_password
---
+++ password.py
@@ -0,0 +1,21 @@
+import random
+
+
+def generate_pass(pass_length):
+    lower = "abcdefghijklmnopqrstuvwxyz"
+    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    digit = "1234567890"
+    special = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+
+    chars = [lower, upper, digit, special]
+
+    password = ""
+
+    for i in range(pass_length):
+        
+        index = random.randint(0,3)
+        size = len(chars[index]) - 1
+        char_index = random.randint(0, size)
+        password = password + chars[index][char_index]
+        
+    return password
